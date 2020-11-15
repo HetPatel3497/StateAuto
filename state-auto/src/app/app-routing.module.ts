e81f8df3f32ca341caaef740b207c3a02c010d69
Het Patel <patel.3497@buckeyemail.osu.edu>
2020-11-14 20:23:49 -0500

    more pages added

--- state-auto/src/app/app-routing.module.ts
+++ state-auto/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { WeatherComponent } from './weather/weather.component';
+import { CrimeComponent } from './crime/crime.component';
+import {CompareComponent} from './compare/compare.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
 {
@@ -17,7 +20,24 @@ const routes: Routes = [
 {
   path: 'weather',
   component: WeatherComponent,
+},
+
+{
+  path: 'crime',
+  component: CrimeComponent,
+},
+
+{
+  path: 'compare',
+  component: CompareComponent,
+},
+
+{
+  path: 'about',
+  component: AboutComponent,
 }
+
+
 
 ];
 
--- state-auto/src/app/app.module.ts
+++ state-auto/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { WeatherService } from './services/weather.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CrimeComponent } from './crime/crime.component';
+import { AboutComponent } from './about/about.component';
+import { CompareComponent } from './compare/compare.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-
+    CrimeComponent,
+    CompareComponent,
+    AboutComponent,
+    
   ],
   imports: [
     BrowserModule,
